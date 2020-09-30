6b03c30b91ebf77f9e071ec4a6768ad8ca6ffff5
diulv <danielclaywillmott@gmail.com>
2020-09-30 14:59:09 -0400

    working log out using selenium

--- less-doom-scrolling.py
+++ less-doom-scrolling.py
@@ -2,15 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-usernameField = find_element_by_name("session[username_or_email]")
-passwordField = find_element_by_name("session[password]")
-
-browser = webdriver.Firefox()
 username = username
 password = password
 
 browser = webdriver.Firefox()
-broswer.get("https://www.twitter.com/login")
+
+browser.get("https://www.twitter.com/login")
 time.sleep(10)
 
 username_field = browser.find_element_by_name("session[username_or_email]")
@@ -19,3 +16,13 @@ password_field = browser.find_element_by_name("session[password]")
 username_field.send_keys(username)
 password_field.send_keys(password)
 username_field.send_keys(Keys.RETURN)
+time.sleep(10)
+
+browser.get("https://twitter.com/settings/applications")
+time.sleep(10)
+
+#parent of span with 'Log out all...' text
+browser.find_element_by_xpath("//*[contains(text(), 'Log out all other sessions')]/parent::*").click()
+
+#confirm log out
+browser.find_element_by_xpath("//div[@data-testid='confirmationSheetConfirm']")
