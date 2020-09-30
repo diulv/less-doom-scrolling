9a29af39f67cdb7acd574d0c781f6520c3ac49fd
diulv <danielclaywillmott@gmail.com>
2020-09-30 08:47:13 -0400

    moved dev to headless selenium

--- less-doom-scrolling.py
+++ less-doom-scrolling.py
@@ -1,24 +1,21 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
-url = 'https://twitter.com/login'
-username = "danielclaywillmott@gmail.com"
-password = 'password'
+usernameField = find_element_by_name("session[username_or_email]")
+passwordField = find_element_by_name("session[password]")
 
+browser = webdriver.Firefox()
+username = username
+password = password
 
-r = requests.get('https://twitter.com/login')
-soup = BeautifulSoup(r.content, "lxml")
-token = soup.select_one("input[name=authenticity_token]")["value"]
-params = {"session[username_or_email]": username,
-        "session[password]": password,
-		"authenticity_token": token}
-r = s.post(url, data=params)
-print(r.content)
+browser = webdriver.Firefox()
+broswer.get("https://www.twitter.com/login")
+time.sleep(10)
 
-
-
-
-
-
-
-#r = requests.get('https://twitter.com/settings/applications')
+username_field = browser.find_element_by_name("session[username_or_email]")
+password_field = browser.find_element_by_name("session[password]")
+        
+username_field.send_keys(username)
+password_field.send_keys(password)
+username_field.send_keys(Keys.RETURN)
