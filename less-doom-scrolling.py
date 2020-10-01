badd8d56cb7edd4c0823f6ddf5e0edff9fa383d2
diulv <danielclaywillmott@gmail.com>
2020-09-30 20:58:29 -0400

    add final click, configure user/pass with netrc

--- less-doom-scrolling.py
+++ less-doom-scrolling.py
@@ -1,14 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import netrc
 import time
 
-username = username
-password = password
+creds = netrc.netrc()
+
+login, username, password = creds.authenticators("twitter")
 
 browser = webdriver.Firefox()
-
 browser.get("https://www.twitter.com/login")
-time.sleep(10)
+time.sleep(5)
 
 username_field = browser.find_element_by_name("session[username_or_email]")
 password_field = browser.find_element_by_name("session[password]")
@@ -16,13 +17,14 @@ password_field = browser.find_element_by_name("session[password]")
 username_field.send_keys(username)
 password_field.send_keys(password)
 username_field.send_keys(Keys.RETURN)
-time.sleep(10)
+time.sleep(5)
 
 browser.get("https://twitter.com/settings/applications")
-time.sleep(10)
+time.sleep(5)
 
 #parent of span with 'Log out all...' text
 browser.find_element_by_xpath("//*[contains(text(), 'Log out all other sessions')]/parent::*").click()
+time.sleep(5)
 
 #confirm log out
-browser.find_element_by_xpath("//div[@data-testid='confirmationSheetConfirm']")
+browser.find_element_by_xpath("//div[@data-testid='confirmationSheetConfirm']").click()
